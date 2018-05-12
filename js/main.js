c02d07ffec572751a7f5091f2809e04158c389c3
eugenius <eugen.cernei@gmail.com>
2018-05-12 21:17:40 +0300

    Added orbit control limitations

--- js/main.js
+++ js/main.js
@@ -5,13 +5,12 @@ window.addEventListener('load', function () {
     var w = window.innerWidth,
         h = window.innerHeight;
 
-    var container, renderer, scene, camera, controls, children;
-    var raycaster, mouse = new THREE.Vector2(), INTERSECTED;
+    var container, renderer, scene, camera, controls;
 
     var container2, renderer2, cam2, controls2, camHelper, stats, isDown = false, isDragging = false;
 
     (function init() {
-        
+
         // Main renderer
         renderer = new THREE.WebGLRenderer({
             antialias: true
@@ -33,61 +32,66 @@ window.addEventListener('load', function () {
         container2 = document.getElementById('container2');
         container2.appendChild(renderer2.domElement);
 
-        
-        // 
         // Debug framerate
-        // 
-
-        stats = new Stats();
-
-        container2.appendChild(stats.dom);
-        stats.dom.style.position = 'absolute';
-
-
+        (function createFramerateDebugger() {
+            stats = new Stats();
+            container2.appendChild(stats.dom);
+            stats.dom.style.position = 'absolute';
+        })()
+        
         // Create world
         scene = new THREE.Scene();
         scene.fog = new THREE.FogExp2(0x55555, 0.0018);
         renderer.setClearColor(scene.fog.color);
         renderer2.setClearColor(scene.fog.color);
 
+        // Create camera
+        function createPerspectiveCamera(x, y, z) {
+            // Create perspective camera at given position
+            const cam = new THREE.PerspectiveCamera(60, w / h, 5, 2000);
+            cam.position.set(x, y, z);
+            cam.lookAt(new THREE.Vector3(0, 0, 0));
+            return cam
+        }
 
         // Create main camera
-        camera = new THREE.PerspectiveCamera(60, w / h, 1, 2000);
-        camera.position.set(25, 20, 100);
-        camera.lookAt(new THREE.Vector3(0, 0, 0));
-
+        camera = createPerspectiveCamera(40, 30, 40);
         camHelper = new THREE.CameraHelper(camera);
         scene.add(camHelper);
 
         // Create preview camera
-        cam2 = new THREE.PerspectiveCamera(60, w / h, 1, 2000);
-        cam2.position.set(0,100,400);
-        cam2.lookAt(new THREE.Vector3(0, 0, 0));
+        cam2 = createPerspectiveCamera(60, 40, 60)
+
         controls2 = new THREE.OrbitControls(cam2, renderer2.domElement);
-    
-        scene.add(new THREE.AxisHelper(50));
+
+        scene.add(new THREE.AxesHelper(50));
         scene.add(new THREE.GridHelper(100, 10));
 
         // Add lighting to the scene
         threePointLight();
-        
+
         controls = new THREE.OrbitControls(camera, renderer.domElement);
-        raycaster = new THREE.Raycaster();
-        children = new THREE.Object3D();
-        
-        
-        var surfaceGeometry = new THREE.PlaneGeometry(100, 100, 32);
-        
-        var texture = new THREE.TextureLoader().load("assets/grass.jpg");
-        texture.wrapS = THREE.RepeatWrapping;
-        texture.wrapT = THREE.RepeatWrapping;
-        texture.repeat.set( 4, 4 );
+        controls.minDistance = 50; 
+        controls.maxDistance = 150;
+        controls.minPolarAngle = 0;
+        controls.maxPolarAngle = Math.PI / 2;
+        controls.enablePan = false;
+        // 10 - 50  ||  150 - 400
 
-        var material = new THREE.MeshBasicMaterial( { map: texture } );
+        (function createGrassPlane() {
+            var surfaceGeometry = new THREE.PlaneGeometry(100, 100, 32);
 
-        var surface = new THREE.Mesh(surfaceGeometry, material);
-        surface.rotation.x = -Math.PI / 2;
-        scene.add(surface);
+            var texture = new THREE.TextureLoader().load("assets/grass.jpg");
+            texture.wrapS = THREE.RepeatWrapping;
+            texture.wrapT = THREE.RepeatWrapping;
+            texture.repeat.set(4, 4);
+
+            var material = new THREE.MeshBasicMaterial({ map: texture });
+
+            var surface = new THREE.Mesh(surfaceGeometry, material);
+            surface.rotation.x = -Math.PI / 2;
+            scene.add(surface);
+        })();
 
         // Add the listeners
         window.addEventListener('resize', onWindowResize, false);
@@ -95,33 +99,15 @@ window.addEventListener('load', function () {
         container.addEventListener('mousedown', onMouseDown, false);
         container.addEventListener('mouseup', onMouseUp, false);
         container.addEventListener('click', onClick, false);
-
-
     })();
 
 
-
-    var loader = new THREE.OBJLoader();
-
-    // load a resource
-    loader.load('./assets/FantasyHouse.obj',
-
-        // called when resource is loaded
-        function (house) {
-
-            scene.traverse(function (sceneChild) {
-                if (sceneChild.type === 'PerspectiveCamera') {
-                    camera = sceneChild;
-                    camera.aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
-                    camera.updateProjectionMatrix();
-                }
-            });
-
-            scene.add(house);
-            house.position.set(0, 2.5, 0);
-            console.log(house.position)
-        }
-    );
+    // Load the house
+    const objLoader = new THREE.OBJLoader();
+    objLoader.load('./assets/FantasyHouse.obj', function (house) {
+        scene.add(house);
+        house.position.set(0, 2.5, 0);
+    });
 
     function onWindowResize() {
         w = window.innerWidth;
@@ -136,40 +122,14 @@ window.addEventListener('load', function () {
         renderer2.setSize(200, 200);
     }
 
-
     function onMouseMove(event) {
         event.preventDefault();
         if (isDown) isDragging = true;
-
-        if (!isDragging) {
-            mouse.x = (event.clientX / w) * 2 - 1;
-            mouse.y = -(event.clientY / h) * 2 + 1;
-
-            raycaster.setFromCamera(mouse, camera);
-            var intersects = raycaster.intersectObjects(children.children);
-            if (intersects.length > 0) {
-                if (INTERSECTED != intersects[0].object) {
-                    if (INTERSECTED) INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
-                    INTERSECTED = intersects[0].object;
-                    INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
-                    INTERSECTED.material.emissive.setHex(0xffff00);
-                    container.style.cursor = 'pointer';
-                }
-            } else {
-                if (INTERSECTED) INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
-                INTERSECTED = null;
-                container.style.cursor = 'auto';
-            }
-        }
     }
-
 
     function onClick(event) {
         event.preventDefault();
-        if (!isDragging && INTERSECTED) {
-            var centroid = bsphere.center;
-            controls.target.copy(centroid);
-            controls.update();
+        if (!isDragging ) {
             controls.update();
         }
     }
@@ -179,20 +139,16 @@ window.addEventListener('load', function () {
         isDown = true;
     }
 
-
     function onMouseUp(event) {
         event.preventDefault();
         isDown = false;
         isDragging = false;
     }
 
-
     (function animate() {
         requestAnimationFrame(animate);
-
         camHelper.visible = false;
         renderer.render(scene, camera);
-
         camHelper.visible = true;
         renderer2.render(scene, cam2);
         stats.update();
